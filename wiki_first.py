8cd5a8a4692bca4e6031f4a7fe9fca6dc5646165
UHIPUser <UHIPUser@DHS-LP-UHIP19.enterprise.ri.gov>
2018-07-19 15:42:39 -0400

    git_manual updated

--- wiki_first.py
+++ wiki_first.py
@@ -2,10 +2,10 @@ import unittest
 from selenium import webdriver
 
 driver = webdriver.Firefox()
-driver.implicity_wait(10)
+driver.implicitly_wait(10)
 driver.maximize_window()
 
-driver.get(https://www.wikipedia.org/)
+driver.get("https://www.wikipedia.org/")
 driver.find_element_by_id("js-link-box-ru").click()
 
 driver.quit()
